f035b12484797967bbbc116c400de55595588580
jesse.vandenkerckhove <jesse.vandenkerckhove@student.howest.be>
2020-07-23 11:08:16 +0200

    Card components working a little better

--- src/assets/card-component.js
+++ src/assets/card-component.js
@@ -1,26 +1,54 @@
-const template = document.createElement('template');
-template.innerHTML = `
+//Card//////////////////////////////////////////////////////////////////
+const cardTemplate = document.createElement("template");
+cardTemplate.innerHTML = `
+<div>
+<style>
+p{
+    background-color: pink;
+}
+</style>
+<p>heyo</p>
+</div>
+`;
+
+class Card extends HTMLElement {
+    constructor() {
+        super();
+        console.log("card ding");
+        this.attachShadow( { mode: "open" });
+        this.shadowRoot.appendChild(cardTemplate.content.cloneNode(true));
+    }
+}
+
+
+
+//Text Card//////////////////////////////////////////////////////////
+const textTemplate = document.createElement('template');
+
+textTemplate.innerHTML = `
 <div>
     <div class="container container--type-one">
         <header>header</header>
         <div class="story-container">
             <p id="text">Default card text</p>
         </div>
-        <div><slot name="options"/></div>
+        <div><slot/></div>
+        <p>tezst</p>
     </div>
 </div>
 `;
-
-class TextCard extends HTMLElement {
+class TextCard extends Card {
     constructor() {
         super();
-        //this.attachShadow( { mode: "open" });
-        this.appendChild(template.content.cloneNode(true));
+        console.log("text card");
+        this.shadowRoot.appendChild(textTemplate.content.cloneNode(true));
 
-        this.querySelector("#text").innerText =
+        this.shadowRoot.querySelector("#text").innerText =
             this.getAttribute("text");
     }
 
 }
+
+
 
 window.customElements.define("text-card", TextCard);
